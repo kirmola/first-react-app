1e859111f3f952fe0dca467ef404bbef977d905f
kirmolaa <kirmolaa@gmail.com>
2024-05-11 02:09:37 +0000

    learnt to display data with the help of escaping tags

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Button = ({sample}) => {
+const Button = () => {
+
+    const user = {
+        name: "Aman",
+        class: "BCA",
+        roll_no: 45
+    }
   return (
-    <button>I am a gyrating pollination soab {sample}</button>
+    <>
+        <button className='testclass'>I am a gyrating pollination soab</button>
+        <div className='anothertestclass'>This data is shown with the help of a dictionary : name: {user.name}, class: {user.class}, roll: {user.roll_no}</div>
+    </>
   )
 }
 
