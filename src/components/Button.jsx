4492366ae3097c7798e4bbb82d94c201eeb4030a
kirmolaa <kirmolaa@gmail.com>
2024-05-11 02:14:20 +0000

    moved data to separate component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 
 const Button = () => {
 
-    const user = {
-        name: "Aman",
-        class: "BCA",
-        roll_no: 45
-    }
   return (
     <>
         <button className='testclass'>I am a gyrating pollination soab</button>
-        <div className='anothertestclass'>This data is shown with the help of a dictionary : name: {user.name}, class: {user.class}, roll: {user.roll_no}</div>
     </>
   )
 }
---
+++ src/components/Data.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Data = () => {
+    const user = {
+        name: "Aman",
+        class: "BCA",
+        roll_no: 45
+    }
+
+  return (
+    <div>
+        <div className='anothertestclass'>This data is shown with the help of a dictionary : name: {user.name}, class: {user.class}, roll: {user.roll_no}</div>
+
+    </div>
+
+  )
+}
+
+export default Data
